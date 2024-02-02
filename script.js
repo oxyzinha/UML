dcdef435b43e5c0ea45e3396a726181e04852bc9
vishwa <vishwarao@epfundao.edu.pt>
2024-02-02 16:02:55 +0000

    Html about UML

---
+++ script.js
@@ -0,0 +1,7 @@
+
+    // Toggle Google Translate
+    function toggleTranslate()
+     {
+        var googleTranslateElement = document.getElementById('google_translate_element');
+        googleTranslateElement.style.display === 'none' ? googleTranslateElement.style.display = 'block' : googleTranslateElement.style.display = 'none';
+    }
